35a53666d8fd4bd067b5c135ef7d122813a1d849
wjdgus95 <wjdguswjdal@naver.com>
2020-09-27 23:54:44 +0900

    springboot 프로젝트 snow board 시작

---
+++ SNOW_BOARD/src/main/java/com/snow/board/SnowBoardApplication.java
@@ -0,0 +1,13 @@
+package com.snow.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowBoardApplication.class, args);
+	}
+
+}
---
+++ SNOW_BOARD/src/test/java/com/snow/board/SnowBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snow.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
